a93326f39f96ed3cdf382ce3e054fe311edad059
Keefe1349 <1298289887@qq.com>
2022-10-15 08:51:38 +0800

    实验

---
+++ 实验三 继承关系/xuanke.java
@@ -0,0 +1,19 @@
+package jicheng;
+
+public class xuanke {
+
+		public static void main(String[] args) {
+			//创建teacher对象
+			teacher t1=new teacher(1,"张三","男");
+			teacher t2=new teacher(2,"李四","女");
+			//创建course对象
+			course c1=new course(1,"数学","教101","8:00",t1);
+			course c2=new course(2,"物理","教303","10:00",t2);
+			//创建student对象
+			students s1=new students(1,"小王","男");
+			s1.xk(c2);
+			System.out.println("学生信息");
+			System.out.println("编号:"+ s1.course.id +"课程名称:"+ s1.course.cname +"时间:"+ s1.course.time+"授课教师:"+s1.course.teacher.name);
+		}
+
+}
